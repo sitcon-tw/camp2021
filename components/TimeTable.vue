b45534cbc7b85d2e6342e223f253a4d6fc583e9f
gnehs <jayuiop@gmail.com>
2021-06-26 00:13:36 +0800

    updated timetable

--- components/TimeTable.vue
+++ components/TimeTable.vue
@@ -1,81 +1,69 @@
 <template>
   <div>
-    <table class="time-table">
-      <thead>
-        <tr>
-          <th>時間 / 日期</th>
-          <th>ㄧ</th>
-          <th>二</th>
-          <th>三</th>
-          <th>四</th>
-          <th>五</th>
-        </tr>
-      </thead>
+    <table border="0" class="time-table">
       <tbody>
         <tr>
-          <td>09:00-10:20</td>
-          <td rowspan="2">開幕</td>
-          <td rowspan="5">主線課程</td>
-          <td rowspan="5">主線課程</td>
-          <td>廣度課程</td>
-          <td rowspan="10">黑客松</td>
+          <td>時間</td>
+          <td>8/11（三）</td>
+          <td>8/12（四）</td>
+          <td>8/13（五）</td>
+          <td>8/14（六）</td>
+          <td>8/15（日）</td>
+          <td>8/16（一）</td>
+          <td>8/17（二）</td>
         </tr>
         <tr>
-          <td>10:20-10:30</td>
-          <td rowspan="4">社群闖關</td>
+          <td>08:40-10:00</td>
+          <td>
+            <p>開幕</p>
+          </td>
+          <td rowspan="2">主線課程</td>
+          <td rowspan="2">主線課程</td>
+          <td rowspan="2">主線課程</td>
+          <td rowspan="2">視界咖啡館</td>
+          <td rowspan="9">黑客松</td>
+          <td rowspan="5">黑客松</td>
         </tr>
         <tr>
-          <td>10:30-11:30</td>
+          <td>10:00-12:00</td>
           <td>認識 Discord Bot</td>
         </tr>
         <tr>
-          <td>11:30-12:00</td>
-          <td>黑客松討論</td>
-        </tr>
-        <tr>
-          <td>12:00-12:20</td>
-          <td rowspan="2">用餐時間</td>
-        </tr>
-        <tr>
-          <td>12:20-13:30</td>
+          <td>12:00-13:30</td>
+          <td>用餐時間</td>
+          <td>用餐時間</td>
           <td>用餐時間</td>
           <td>用餐時間</td>
           <td>用餐時間</td>
         </tr>
         <tr>
           <td>13:30-14:50</td>
-          <td rowspan="2">主線課程</td>
+          <td rowspan="4">主線課程</td>
           <td>廣度課程</td>
+          <td>神秘活動</td>
           <td>開源精神短講</td>
-          <td>社群闖關</td>
+          <td rowspan="3">密室逃脫</td>
         </tr>
         <tr>
-          <td>14:50-16:30</td>
-          <td rowspan="2">主線課程</td>
-          <td rowspan="2">主線課程</td>
-          <td rowspan="2">視界咖啡館</td>
+          <td>14:50-15:00</td>
+          <td rowspan="5">主線課程</td>
+          <td rowspan="5">主線課程</td>
+          <td rowspan="5">社群闖關</td>
         </tr>
         <tr>
-          <td>16:30-17:30</td>
-          <td>認識 Discord Bot</td>
+          <td>15:00-16:40</td>
+          <td rowspan="3">黑客松發表</td>
         </tr>
         <tr>
-          <td>17:30-18:30</td>
-          <td>用餐時間</td>
-          <td>用餐時間</td>
-          <td>用餐時間</td>
-          <td>用餐時間</td>
+          <td>16:40-16:50</td>
+          <td rowspan="3">黑客松</td>
         </tr>
         <tr>
-          <td>18:30-20:30</td>
-          <td rowspan="2">主線課程</td>
-          <td rowspan="2">密室逃脫</td>
-          <td rowspan="2">黑客松</td>
-          <td rowspan="2">黑客松</td>
-          <td>黑客松發表</td>
+          <td>16:50-17:40</td>
+          <td rowspan="2">廣度課程</td>
         </tr>
         <tr>
-          <td>20:30-21:40</td>
+          <td>17:40-18:00</td>
           <td>閉幕</td>
         </tr>
       </tbody>
@@ -97,7 +85,7 @@ export default {};
         background-color: rgb(40 89 119 / 25%)
         text-align: center
         height: 2em
-        width: calc(100% / 6)
+        width: calc(100% / 8)
         &:hover
             background-color: rgb(40 89 119 / 30%)
 
