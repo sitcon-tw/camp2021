eea2f9a29530627e44ae6ab13a18c7411463070d
Denny Huang <denny0223@gmail.com>
2021-06-27 21:42:53 +0800

    Update page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'SITCON Camp Online 2021',
+    title: 'SITCON 線上夏令營 2021 | SITCON Camp Online',
     htmlAttrs: {
       lang: 'zh-TW'
     },
