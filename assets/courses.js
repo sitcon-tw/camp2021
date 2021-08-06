6b2bf8109ef3a80fc6accbb242b23b158fd9e073
gnehs <jayuiop@gmail.com>
2021-08-06 22:00:45 +0800

    added course-info dialog

---
+++ assets/courses.js
@@ -0,0 +1,40 @@
+export default {
+    courses: {
+        和飛船保持社交距離: {
+            subtitle: '基礎遠端操作與編輯器教學',
+            description: '一開始會講一些基礎 Linux、Vim 的操作，主要時間圍繞在基礎的 Python 語法跟練習，用練習讓學員快速熟悉語法。',
+            speakers: ['橋毅']
+        },
+        初出茅廬: {
+            subtitle: 'Python 3',
+            description: '這堂課主要的內容是迴圈跟容器的部分，一樣用大量的練習讓學員可以活用迴圈跟容器的操作。',
+            speakers: ['橋毅']
+        },
+        星際間的通訊: {
+            subtitle: 'HTTP / RESTful',
+            description: '每天你在手機滑著 Instagram，又或是在電腦上打開瀏覽器逛著各種網頁，你是否有想過這些內容是如何呈現在你的眼前？ 聽到伺服器這個詞，你想到了什麼？架伺服器真的有這麼困難？到底伺服器要做什麼事情？ 這堂課程將帶你看透一切，過去對你來說或許是黑魔法的未知領域，在課程過後相信你對世界會有嶄新的看法！',
+            speakers: ['丹丹']
+        },
+        義大利麵令人戒慎恐懼: {
+            subtitle: 'Python 中的 Function 與 Module',
+            description: '是不是常常寫了一堆 code 後發現程式結構雜亂無章？每次寫到一個段落休息一下後回來卻發現忘了自己在寫什麼？同一份檔案中參雜了許多用途不同的程式碼，每次想 debug 都找很久？<br>Functions and modules come to the rescue!<br>這堂課會教你在 Python 中使用 Function 以及 Module，讓你的程式分工更明確、開發更有邏輯！',
+            speakers: ['CSY']
+        },
+        在巨人的右肩乘涼: {
+            subtitle: '常用的 Python 套件',
+            description: '概述Python套件功能及常用到的Python套件，讓你在寫程式的時候更加輕鬆😉',
+            speakers: ['康喔']
+        },
+        凝聚開源能量吧: {
+            subtitle: '手把手建立一個 Discord Bot',
+            description: '綜合前幾堂課的教學，帶你從環境的建立、專案的初始化到 discord.py 套件的使用，從零到有做出屬於你的 Discord Bot 吧！',
+            speakers: ['CSY', '康喔']
+        },
+    },
+    speakers: {
+        橋毅: '哈溜，我是橋毅，目前是嘉義高中高三生。',
+        丹丹: 'SITCON 學生計算機年會共同發起人，目前於雷亞遊戲（Rayark Inc.）擔任 Data Team Lead；在正規教育中特例獨行，妄想透過改善教育讓台灣更進步，崇尚開源精神，熱愛使用者界面以外的多項技術。',
+        CSY: '目前就讀於<span style="color: #0000">陽明</span>交通大學，即將成為 Suffermore。正在朝著 Full Stack Developer 邁進，最愛的編（I）輯（D）器（E）是 Vim。剛踏入資訊安全的大門，偶爾會打打 CTF，歡迎交流><',
+        康喔: '嗨嗨，我是康喔，一個在台科爆肝的大二生，對什麼感興趣就會沒日沒夜的研究，這就是爆肝的開頭吧？'
+    }
+}
--- components/TimeTable.vue
+++ components/TimeTable.vue
@@ -18,8 +18,8 @@
             <p>開幕</p>
           </td>
           <td rowspan="2">主線課程</td>
-          <td rowspan="2">主線課程</td>
-          <td rowspan="2">主線課程</td>
+          <td rowspan="2" @click="showCourseInfo('義大利麵令人戒慎恐懼')" class="clickable">義大利麵令人戒慎恐懼</td>
+          <td rowspan="2" @click="showCourseInfo('凝聚開源能量吧')" class="clickable">凝聚開源能量吧</td>
           <td rowspan="2">視界咖啡館</td>
           <td rowspan="9">黑客松</td>
           <td rowspan="5">黑客松</td>
@@ -38,7 +38,7 @@
         </tr>
         <tr>
           <td>13:30-14:50</td>
-          <td rowspan="4">主線課程</td>
+          <td rowspan="4" @click="showCourseInfo('和飛船保持社交距離')" class="clickable">和飛船保持社交距離</td>
           <td>廣度課程</td>
           <td>神秘活動</td>
           <td>開源精神短講</td>
@@ -46,8 +46,8 @@
         </tr>
         <tr>
           <td>14:50-15:00</td>
-          <td rowspan="5">主線課程</td>
-          <td rowspan="5">主線課程</td>
+          <td rowspan="5" @click="showCourseInfo('星際間的通訊')" class="clickable">星際間的通訊</td>
+          <td rowspan="5" @click="showCourseInfo('在巨人的右肩乘涼')" class="clickable">在巨人的右肩乘涼</td>
           <td rowspan="5">社群闖關</td>
         </tr>
         <tr>
@@ -68,28 +68,94 @@
         </tr>
       </tbody>
     </table>
+    <Dialog v-model="courseInfoDialog">
+      <div class="course-title">{{ courseInfoData.title }}</div>
+      <div class="course-subtitle">{{ courseInfoData.subtitle }}</div>
+      <div class="course-info-title">課程介紹</div>
+      <div class="course-description" v-html="courseInfoData.description" />
+      <div v-for="({ name, description }, i) of courseInfoData.speakers" :key="i">
+        <div class="course-info-title">講者介紹 - {{ name }}</div>
+        <div class="course-description" v-html="description" />
+      </div>
+    </Dialog>
   </div>
 </template>
 
 <script>
-export default {};
+import courseData from '@/assets/courses.js'
+let { courses, speakers } = courseData
+export default {
+  data: () => ({
+    courses,
+    speakers,
+    courseInfoDialog: false,
+    courseInfoData: {
+      title: '勝勝可愛',
+      subtitle: '蓬蓬鬆餅好好ㄘ',
+      description: '喵喵喵！',
+      speakers: [
+        {
+          name: '蓬蓬鬆餅',
+          description: '～世人都愛的美味鬆餅～'
+        }
+      ]
+    }
+  }),
+  methods: {
+    showCourseInfo(id) {
+      console.log(this.speakers)
+      let { subtitle, description, speakers } = this.courses[id]
+      speakers = speakers.map(x => ({ name: x, description: this.speakers[x] }))
+      this.courseInfoData = {
+        title: id,
+        subtitle,
+        description,
+        speakers
+      }
+      this.courseInfoDialog = true
+    }
+  }
+}
 </script>
 <style lang="sass">
+.course-title
+  font-size: 40px
+  line-height: 1.5em
+  font-weight: bold
+.course-subtitle
+  font-size: 32px
+  line-height: 1.5em
+  color: #E0E0E0
+  padding-bottom: 16px
+  margin-bottom: 16px
+  border-bottom: 1px solid #E0E0E0
+.course-info-title
+  font-size: 24px
+  line-height: 1.5em
+  font-weight: bold
+  margin: 16px 0
+  margin-top: 32px
+.course-description
+  font-size: 20px
+  line-height: 1.5em
 .time-table
-    width: 100%
-    border-radius: 16px
-    overflow: hidden
-    th
-        background-color: rgb(40 89 119 / 40%)
-    td
-        background-color: rgb(40 89 119 / 25%)
-        text-align: center
-        height: 2em
-        width: calc(100% / 8)
-        &:hover
-            background-color: rgb(40 89 119 / 30%)
+  width: 100%
+  border-radius: 16px
+  overflow: hidden
+  th
+    background-color: rgb(40 89 119 / 40%)
+  td
+    background-color: rgb(40 89 119 / 25%)
+    text-align: center
+    height: 2em
+    width: calc(100% / 8)
+    &.clickable
+      cursor: pointer
+      text-decoration: underline
+    &:hover
+      background-color: rgb(40 89 119 / 30%)
 
 @media screen and (max-width: 512px)
-    .time-table
-        font-size: 12px
+  .time-table
+    font-size: 12px
 </style>
